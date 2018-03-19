c17c3d559c162a5e9527b2cdf68314f21456e547
Ali Mustofa <mustofa@extrainteger.com>
2018-03-19 14:08:07 +0700

    chenge flow

--- identitaslogin/src/main/java/com/extrainteger/identitaslogin/IdentitasConstants.kt
+++ identitaslogin/src/main/java/com/extrainteger/identitaslogin/IdentitasConstants.kt
@@ -5,21 +5,20 @@ package com.extrainteger.identitaslogin
  */
 class IdentitasConstants {
     companion object {
-        val LOGIN_URL_FIELD = "LOGIN_URL"
-        val LOGIN_URL_VALUE = "https://identitas.extrainteger.com/oauth/authorize"
-        val LOGIN_ACTIVITY_REQUEST_CODE = 212
-        val CLIENT_ID_FIELD = "client_id"
-        val CLIENT_SECRET_FIELD = "client_secret"
-        val REDIRECT_URI_FIELD = "redirect_uri"
-        val SCOPE_FIELD = "scope"
-        val RESPONSE_TYPE_FIELD = "response_type"
-        val RESPONSE_TYPE_VALUE = "code"
-        val BASE_URL_VALUE = "https://identitas.extrainteger.com"
-        val GRANT_TYPE = "authorization_code"
-        val ACCESS_TOKEN_FIELD = "access_token"
-        val REFRESH_TOKEN_FIELD = "refresh_token"
-        val TOKEN_TYPE_FIELD = "token_type"
-        val GET_TOKEN_ERROR_CODE_FIELD = "get_token_error_code"
-        val GET_TOKEN_EXCEPTION_FIELD = "get_token_exception"
+            val LOGIN_ACTIVITY_REQUEST_CODE = 212
+            val CLIENT_ID_FIELD = "client_id"
+            val CLIENT_SECRET_FIELD = "client_secret"
+            val REDIRECT_URI_FIELD = "redirect_uri"
+            val SCOPE_FIELD = "scope"
+            val RESPONSE_TYPE_FIELD = "response_type"
+            val RESPONSE_TYPE_VALUE = "code"
+            val GRANT_TYPE = "authorization_code"
+            val ACCESS_TOKEN_FIELD = "access_token"
+            val REFRESH_TOKEN_FIELD = "refresh_token"
+            val TOKEN_TYPE_FIELD = "token_type"
+            val GET_TOKEN_ERROR_CODE_FIELD = "get_token_error_code"
+            val GET_TOKEN_EXCEPTION_FIELD = "get_token_exception"
+            val BASE_URL_FIELD = "base_url"
+            val REFERER_FIELD = "referer"
     }
 }
